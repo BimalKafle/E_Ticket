fe61c8fdcb24adbe589c2490a1dd791f08e4f8aa
BimalKafle <bkafle25@gmail.com>
2022-07-24 13:50:29 +0545

    small changes made in program

--- E_Ticketing/Program.cs
+++ E_Ticketing/Program.cs
@@ -1,10 +1,25 @@
 using E_Ticketing.DataLayer.DataAccess;
 using E_Ticketing;
+using E_Ticketing.CoreLayer.Entity;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddIdentity<User, IdentityRole>();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 4;
+    options.Password.RequireDigit = false;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.SignIn.RequireConfirmedEmail = false;
+
+});
 builder.Services.UseETicketing();
 var app = builder.Build();
 
